e3a99d191f8c8b22b04dc1ef71c118808f74898a
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-05 18:16:06 +0200

    ok

---
+++ src/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <a href="">logo</a>
+      <nav>
+          naw items
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
+
+<style>
+
+</style>
