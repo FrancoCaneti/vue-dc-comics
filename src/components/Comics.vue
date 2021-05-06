af609f781fc076639017a8146e02695bbdecb706
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-06 13:19:54 +0200

    fumetti main

--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -1,16 +1,18 @@
 <template>
-   <div class="fumetto">
-       <img :src="details.scr" :alt="details.series">
-   </div>
+  <div class="fumetti">
+    <img :src="details.thumb" :alt="details.series" />
+  </div>
 </template>
 
 <script>
 export default {
-name:'Comics',
-props:['details'],
-}
+  name: "Comics",
+  props: ["details"],
+};
 </script>
 
 <style>
-
+.fumetti {
+  display: flex;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,46 +1,48 @@
 <template>
   <header>
-      <a href="/">
-      <img src="../assets/dc-logo.png" alt="">
-      </a>
-        <ul>
-          <li>CHARACTERS</li>
-           <li>COMICS </li>
-            <li>MOVIES</li>
-             <li>TV</li>
-              <li>GAMES</li>
-               <li>COLLECTIBLES</li>
-                <li>VIDEOS</li>
-                <li>FANS</li>
-                <li>NEWS</li>
-                <li>SHOP</li>
-       </ul>
+    <a href="/">
+      <img src="../assets/dc-logo.png" alt="" />
+    </a>
+    <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
   </header>
 </template>
 
 <script>
 export default {
- name:'Header'
-}
+  name: "Header",
+};
 </script>
 
 <style scoped>
-header{
+header {
   display: inline-flex;
-  margin-top:30px;
-  margin-left:200px;
+  margin-top: 30px;
+  margin-left: 200px;
   font-family: sans-serif;
 }
-a{
+a {
   margin-right: 100px;
-  height:30px
+  height: 30px;
 }
 
-li{
+li {
   list-style: none;
   display: inline-block;
-  margin:10px
+  margin: 10px;
 }
-
-
+.container {
+  width: 100%;
+  background-image: url(../assets/jumbotron.jpg);
+}
 </style>
